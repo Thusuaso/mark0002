1b7494eb91c803b0cf4d5aa56c1cca3e780da31f
ferdiinan <inan79@gmail.com>
2024-02-12 10:05:02 +0300

    l

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,7 +70,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     //baseURL: 'http://localhost:3000/api'
-    baseURL:'http://157.230.115.223/api',
+    baseURL:'http://159.89.20.40/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
