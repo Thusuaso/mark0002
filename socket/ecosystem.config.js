f1e97fb12b4e9778cf28c8961edbb04231c04ad6
Thusuaso <bilgiislem@mekmar.com>
2024-10-05 14:32:23 +0300

    l

--- socket/ecosystem.config.js
+++ socket/ecosystem.config.js
@@ -1,21 +1,10 @@
 module.exports = {
-    apps: [
-      {
-        name: 'SocketIO',
-        exec_mode: 'cluster',
-        instances: '1',
-        script: './api.js',
-        env : {
-
-          "PORT" : 3001
-
-      },
-      env_production: {
-
-        "PORT" : 3001
-
+  apps: [
+    {
+      name: 'SocketIO',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './api.js',
     }
-      },
-      
-    ]
-  }
+  ]
+}
